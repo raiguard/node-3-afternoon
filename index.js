1bdaadb241ce8cf6011c3b6575956a8c386a1aea
raiguard <caleb.heuer@gmail.com>
2020-07-01 14:46:18 -0600

    Step 2

--- index.js
+++ index.js
@@ -3,9 +3,21 @@ require("dotenv").config();
 const express = require("express"),
   massive = require("massive");
 
-const { SERVER_PORT } = process.env;
-
 const app = express();
+
+const { SERVER_PORT, CONNECTION_STRING } = process.env;
+
+massive({
+  connectionString: CONNECTION_STRING,
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
+  .then((db) => {
+    app.set("db", db);
+    console.log("Established database connection");
+  })
+  .catch((err) => console.log(err));
 
 app.use(express.json());
 
