44a08e0aa19d66f4249834e952b306f234edbdde
raiguard <caleb.heuer@gmail.com>
2020-07-01 15:37:25 -0600

    Step 5

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 
 const express = require("express"),
-  massive = require("massive");
+  massive = require("massive"),
+  cont = require("./productsController");
 
 const app = express();
 
@@ -20,5 +21,11 @@ massive({
   .catch((err) => console.log(err));
 
 app.use(express.json());
+
+app.get("/api/products", cont.getAll);
+app.get("/api/product/:id", cont.getOne);
+app.put("/api/product/:id", cont.update);
+app.post("/api/product", cont.create);
+app.delete("/api/product/:id", cont.delete);
 
 app.listen(SERVER_PORT, () => console.log(`Server is listening on port ${SERVER_PORT}`));
